170769b62a21bf092e5bf4f099c04b344d72079e
nailseong <nnis9857@gmail.com>
2020-09-13 22:04:47 +0900

    React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../Styles/GlobalStyle";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
 
-function App() {
-  return <div className="App" />;
-}
-
-export default App;
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <>
+      <GlobalStyle />
+      <Router isLoggedIn={!false} />
+    </>
+  </ThemeProvider>
+);
